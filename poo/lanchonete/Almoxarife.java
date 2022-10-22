b841a6586cd3056574a6133919fe34019f9ab227
jefersonsavio <alukardlegend@outlook.com>
2022-10-22 01:32:53 -0300

    comi

---
+++ poo/lanchonete/Almoxarife.java
@@ -0,0 +1,24 @@
+package poo.lanchonete;
+
+public class Almoxarife {
+
+    public class Almoxarife {
+	
+        private void controlarEntrada() {
+            System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+        }
+        private void controlarSaida() {
+            System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+        }
+        void entregarIngredientes() {
+            System.out.println("ENTREGANDO INGREDIENTES");
+            controlarSaida();
+        }
+        void trocarGas() {
+            System.out.println("ALMOXARIFE TROCANDO O GÁS");
+        }
+    
+    }
+    
+    
+}
---
+++ poo/lanchonete/Estabelecimento.java
@@ -0,0 +1,30 @@
+package poo.lanchonete;
+
+public class Estabelecimento {
+
+    public class estabelecimento {
+	
+        public static void main(String[] args) {
+            Cozinheiro cozinheiro = new Cozinheiro();
+            
+            //ações que estabelecimento precisa ter ciência
+            cozinheiro.adicionarSucoNoBalcao();;
+            cozinheiro.adicionarLancheNoBalcao();
+            cozinheiro.adicionarComboNoBalcao();
+            
+            
+            Atendente atendente = new Atendente();
+            atendente.receberPagamento();
+            atendente.servindoMesa();
+            
+            Cliente cliente = new Cliente();
+            cliente.escolherLanche();
+            cliente.fazerPedido();
+            cliente.pagarConta();
+            
+            
+            
+        }
+    
+    
+}
